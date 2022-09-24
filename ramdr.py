e5d3e4d43f7a686966a64772e3289b0b5482eddf
Noah Cohn <noahc42@gmail.com>
2022-09-23 17:02:27 -0700

    Create ramdr.py

---
+++ ramdr.py
@@ -0,0 +1,13 @@
+import json
+import os
+import socket
+import string
+import sys
+def create(name, size):
+  os.mkdir(f'/mnt/{name}')
+  mib_in_mb = 0.95367431640625
+  size_mebibytes = int(int(size)*mib_in_mb)+1
+  fstab_entry=f'{name} /mnt/{name} tmpfs defaults,size={size_mebibytes}M,x-gvfs-show 0 0'
+  with open('/etc/fstab','a') as f:
+    f.write(fstab_entry+'\n')
+  os.system(f'mount /mnt/{name}')
