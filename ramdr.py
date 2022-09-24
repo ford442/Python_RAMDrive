54aa711e8dbf546dcf5dbb76e482a86b28773b88
Noah Cohn <noahc42@gmail.com>
2022-09-23 19:30:37 -0700

    Update ramdr.py

--- ramdr.py
+++ ramdr.py
@@ -7,7 +7,7 @@ def create(name, size):
   os.mkdir(f'/mnt/{name}')
   mib_in_mb = 0.95367431640625
   size_mebibytes = int(int(size)*mib_in_mb)+1
-  fstab_entry=f'{name} /mnt/{name} tmpfs defaults,size={size_mebibytes}M,x-gvfs-show 0 0'
+  fstab_entry=f'{name} /mnt/{name} ramfs nodev,nosuid,noexec,nodiratime,size={size_mebibytes}M,x-gvfs-show 0 0'
   with open('/etc/fstab','a') as f:
     f.write(fstab_entry+'\n')
   os.system(f'mount /mnt/{name}')
