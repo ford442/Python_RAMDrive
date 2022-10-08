2bbb90850b78da8cbc055199d10431207c1488ac
Noah Cohn <noahc42@gmail.com>
2022-10-07 23:30:20 -0700

    Update ramdr.py

--- ramdr.py
+++ ramdr.py
@@ -4,10 +4,10 @@ import socket
 import string
 import sys
 def create(name, size):
-  os.mkdir(f'/mnt/{name}')
+  os.mkdir(f'/content/{name}')
   mib_in_mb = 0.95367431640625
   size_mebibytes = int(int(size)*mib_in_mb)+1
-  fstab_entry=f'{name} /mnt/{name} ramfs nodev,nosuid,noexec,nodiratime,size={size_mebibytes}M,x-gvfs-show 0 0'
+  fstab_entry=f'{name} /content/{name} ramfs nodev,nosuid,noexec,nodiratime,size={size_mebibytes}M,x-gvfs-show 0 0'
   with open('/etc/fstab','a') as f:
     f.write(fstab_entry+'\n')
-  os.system(f'mount /mnt/{name}')
+  os.system(f'mount /content/{name}')
