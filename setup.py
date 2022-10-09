a4d8358eea1b68f1ace2ed17d6ce0dbbcba36eb7
Noah Cohn <noahc42@gmail.com>
2022-10-09 08:31:04 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+from setuptools import Extension,setup; 
+from Cython.Build import cythonize;
+from Cython.Compiler import Options
+Options.infer_types = True
+Options.language_level = 3
+extensions = [Extension('ramdr',['ramdr.py'])];
+setuptools.setup(
+     name='ramdr',  
+     version='1.0',
+     description="Python ramdrive creator",
+     packages=['ramdr'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: BSD License",
+         "Operating System :: OS Independent",
+     ],
+    ext_modules=cythonize(extensions),
+ )
